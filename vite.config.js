dc7bed6a052ac9dc0e22a50c8813e5fd38f529b8
enzoguil <guillemetenzo2@gmail.com>
2025-06-06 15:56:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/skills-connect/',
+  base: '/skillsconnect/',
   plugins: [react()],
   resolve: {
     alias: {
